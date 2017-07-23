dc670e180d3ab0b11bb5f5af02283a5f5a580f5e
Naren <naren@narendasan.com>
2017-07-23 00:39:29 -0700

    working frontend

--- src/app/App.js
+++ src/app/App.js
@@ -1,51 +1,65 @@
+import dotenv from 'dotenv';
+import axios from 'axios';
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Query from './query/Query.js'
 import Grid from './grid/Grid.js'
 
+dotenv.config()
+
 class App extends Component {
     constructor() {
         super();
-        this.state = {req: {"job_id": 9219219,
-                            "grid": [[{"color": null,
-                                       "text": null},
-                                      {"color": "green",
-                                       "text": "GREEN"},
-                                      {"color": null,
-                                       "text": null}],
-                                      [{"color": "red",
-                                       "text": "BLUE"},
-                                      {"color": null,
-                                       "text": null},
-                                      {"color": "blue",
-                                       "text": "RED"}],
-                                      [{"color": null,
-                                       "text": null},
-                                      {"color": null,
-                                       "text": null},
-                                      {"color": null,
-                                       "text": null}]],
-                            "query": "Pick the red box",
-                            "time": 5000}};
 
+        this.state = {req: {"job_id": null,
+                            "grid": null,
+                            "query": null,
+                            "time": null}};
+
+        alert("You will have 5 seconds to complete the task")
+        this.Job();
         this.click = this.click.bind(this);
+    }
+
+    Job() {
+        axios.get(process.env.REACT_APP_JOB_DISPATCH_URL)
+            .then((res) => {
+                var data = JSON.parse(res.data);
+                if (data.error === "queue_empty") {
+                    return;
+                }
+                this.setState((prevState, props) => {
+                    console.log("NEW TASK");
+                    return {req: data};
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
 
     click(loc, clickable) {
         //RETURN THE SELECTION TO SERVER 
         if (clickable) { 
-            console.log(
-              {
-                "job_id": this.state.req.job_id,
-                "selection": loc
-              }
-          )
+            axios.post(process.env.REACT_APP_JOB_RESPONSE_URL, {
+                job_id: this.state.req.job_id,
+                selected: loc
+            }).then((res) => {
+                if(res.data.status === 'OK') {
+                    this.Job();
+                } else {
+                    console.log("Failed to Submit, please reload");
+                }
+            }).catch((err) => {
+                console.log("Failed to Submit, please reload. ERROR:" + err);
+            });
         }
     }
 
     render() {
-        alert("You will have 5 seconds to complete the task")
+        if (this.state.req.job_id === null) {
+            return (<div>Loading</div>);
+        } 
         return ( 
             <div className="Bender">
                 <Query query={this.state.req.query}/> 
